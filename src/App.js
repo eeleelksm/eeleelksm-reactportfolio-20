43d1792d7ff291fd4f821f2e03d2a7d617d9598d
Keshia-Lee Martin <keshialeemartin@gmail.com>
2022-01-11 14:46:38 -0500

    adding handleChange and onSubmit to contact

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Nav from "./components/Navigation";
 import Header from "./components/Header";
 import About from "./components/About";
 import Portfolio from "./components/Project";
+import ContactForm from "./components/Contact";
 
 function App() {
 	return (
 		<>
 			<Header />
+
 			<main>
+				<ContactForm />
 				<About />
 				<Portfolio />
 			</main>
