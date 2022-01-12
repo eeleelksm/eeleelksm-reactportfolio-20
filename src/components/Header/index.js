5590f9078482cdc4573f77dec767302a48d4ed2d
Keshia-Lee Martin <keshialeemartin@gmail.com>
2022-01-12 13:46:18 -0500

    about me styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,11 +4,15 @@ import Nav from "../Navigation";
 function Header() {
 	return (
 		<header>
-			<section className="header-title">
-				<a href="/">Keshia-Lee Martin</a>
-				<h3>Fullstack Web Developer || Future Game Programmer</h3>
+			<section>
+				<a href="/" className="header-title">
+					Keshia-Lee Martin
+				</a>
+				<h3 className="header-desc">
+					Fullstack Web Developer || Future Game Programmer
+				</h3>
 			</section>
-			<section className="header-nav">
+			<section>
 				<Nav />
 			</section>
 		</header>
--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -3,17 +3,22 @@ import avatarImage from "../../assets/images/avatar-bust.png";
 
 function About() {
 	return (
-		<section>
-			<img className="avatar" src={avatarImage} style={{ width: "65%" }}></img>
-			<p>
-				Keshia-Lee is a Fullstack Web Developer with hopes of transferring her
-				newfound skills into programming and designing video games.
-			</p>
-			<p>
-				She grabs her inspiration from sci-fi and fantasy books. Some of her
-				favorite games are Skyrim, Horizon Zero Dawn. Kingdom Hearts franchise and
-				The Sims franchise.
-			</p>
+		<section className="about-box">
+			<h1>About Me</h1>
+			<div className="about-me">
+				<img className="avatar" src={avatarImage} style={{ width: "40%" }}></img>
+				<div className="about-text">
+					<p>
+						Keshia-Lee is a Fullstack Web Developer with hopes of transferring her
+						newfound skills into programming and designing video games.
+					</p>
+					<p>
+						She grabs her inspiration from sci-fi and fantasy books. Some of her
+						favorite games are Skyrim, Horizon Zero Dawn. Kingdom Hearts franchise and
+						The Sims franchise.
+					</p>
+				</div>
+			</div>
 		</section>
 	);
 }
