d63a288c6d7187b10029560802fb369da3cd1b4b
Keshia-Lee Martin <keshialeemartin@gmail.com>
2022-01-12 18:03:57 -0500

    added projects and resume text

--- src/components/Pages/Project.js
+++ src/components/Pages/Project.js
@@ -1,12 +1,22 @@
 import React from "react";
-import project from "../../assets/images/project-1.png";
+import project1 from "../../assets/images/project-1.png";
+import project2 from "../../assets/images/project-2.png";
+import project3 from "../../assets/images/project-3.png";
+import project4 from "../../assets/images/project-4.png";
+import project5 from "../../assets/images/project-5.png";
+import project6 from "../../assets/images/project-6.png";
 
 function Project() {
 	return (
-		<section>
+		<section className="project-section">
 			<h1>Projects</h1>
 			<div>
-				<img src={project} alt="project name" className="img-thumbnail" />
+				<img src={project1} alt="project-name" className="img-thumbnail" />
+				<img src={project2} alt="project-name" className="img-thumbnail" />
+				<img src={project3} alt="project-name" className="img-thumbnail" />
+				<img src={project4} alt="project-name" className="img-thumbnail" />
+				<img src={project5} alt="project-name" className="img-thumbnail" />
+				<img src={project6} alt="project-name" className="img-thumbnail" />
 			</div>
 		</section>
 	);
--- src/components/Pages/Resume.js
+++ src/components/Pages/Resume.js
@@ -1,7 +1,71 @@
 import React from "react";
 
 function Resume() {
-	return;
+	return (
+		<section>
+			<h1>Resume</h1>
+			<div className="resume">
+				<div className="resume-list">
+					<div className="experience">
+						<h2 className="resume-title">Experience</h2>
+						<p>
+							<b className="job-title">TapSEC Consulting, LLC</b> :: Marketing
+							Assistant
+							<br />
+							<i>FEBRUARY 2019 - PRESENT</i>
+							<br /> <br />
+							<b className="job-title">US Tech Solutions (Google Contract)</b> :: Lead
+							QA Operations Analyst
+							<br />
+							<i>JANUARY 2020 - JULY 2021</i>
+							<br /> <br />
+							<b className="job-title">Crowdstaffing (Google Contract) </b> :: QA Data
+							Analyst
+							<br />
+							<i>AUGUST 2018 - NOVEMBER 2018</i>
+							<br /> <br />
+							<b className="job-title">Elite Personnel</b> :: Quality Control Assistant
+							<br />
+							<i>MAY 2017 - AUGUST 2018</i>
+							<br /> <br />
+						</p>
+					</div>
+					<div className="education">
+						<h2 className="resume-title">Education</h2>
+						<p>
+							<b className="school-title">The George Washington University</b> ::
+							Coding Bootcamp Student
+							<br />
+							<i>AUGUST 2021 - FEBRUARY 2022, REMOTE - CERTIFICATE PENDING</i>
+							<br /> <br />
+							<b className="school-title">YellowBrick</b> :: FIT Gaming and Esports
+							Industry Essential Certificate
+							<br />
+							<i>DECEMBER 2021 - NO EXPIRATION DATE</i>
+							<br /> <br />
+							<b className="school-title">American University </b> :: Bachelor of
+							Science
+							<br />
+							MAJOR: Statistics MINOR: Business Administration
+							<br />
+							<i>AUGUST 2007 - AUGUST 2014</i>
+							<br /> <br />
+						</p>
+					</div>
+					<div className="playtesting">
+						<h2 className="resume-title">Play Testing</h2>
+						<p>
+							<b className="play-title">AOV: Arena of Valor</b> :: Multiplayer Open
+							Battle Arena (MOBA) Mobile Game
+							<br />
+							<i>DECEMBER 2021</i>
+							<br /> <br />
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default Resume;
