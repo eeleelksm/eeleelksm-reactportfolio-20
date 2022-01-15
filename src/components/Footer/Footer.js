f8a12199a30f2e5cf83a4d5b6ce6d5c9f1786ed7
Keshia-Lee Martin <keshialeemartin@gmail.com>
2022-01-15 15:34:55 -0500

    all pages styling completed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,14 +2,27 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
 
 function Footer() {
+	const openInNewTab = (url) => {
+		const newWindow = window.open(url, "_blank", "noopener, norefferer");
+		if (newWindow) newWindow.opener = null;
+	};
+
 	return (
 		<footer className="footer">
 			<div className="itemContainer">
-				<GitHubIcon className="icon" fontSize="large" />
+				<GitHubIcon
+					className="icon"
+					fontSize="large"
+					onClick={() => openInNewTab("https://www.github.com/eeleelksm")}
+				/>
 				<span>Github</span>
 			</div>
 			<div className="itemContainer">
-				<EmailIcon className="icon" fontSize="large" />
+				<EmailIcon
+					className="icon"
+					fontSize="large"
+					onClick={() => openInNewTab("mailto:keshialeemartin@gmail.com")}
+				/>
 				<span>Email</span>
 			</div>
 
--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
 
 function ContactForm() {
 	const [formState, setFormState] = useState({
@@ -20,37 +22,61 @@ function ContactForm() {
 	return (
 		<section>
 			<h1 className="page-title">contact me</h1>
-			<div className="icons"></div>
-			<form id="contact-form" onSubmit={handleSubmit}>
-				<div>
-					<label htmlFor="name">Name:</label>
-					<input
-						type="text"
-						defaultValue={name}
-						onChange={handleChange}
-						name="name"
-					/>
-				</div>
-				<div>
-					<label htmlFor="email">Email Address:</label>
-					<input
-						type="email"
-						defaultValue={email}
-						onChange={handleChange}
-						name="email"
-					/>
-				</div>
-				<div>
-					<label htmlFor="message">Message:</label>
-					<textarea
-						defaultValue={message}
-						onChange={handleChange}
-						name="message"
-						rows="3"
-					/>
-				</div>
-				<button type="submit">Submit</button>
-			</form>
+			<div className="contact-icons">
+				<GitHubIcon className="icon2" />{" "}
+				<a
+					href="https://github.com/eeleelksm"
+					target="_blank"
+					className="contact-link"
+				>
+					eeleelksm
+				</a>
+			</div>
+			<div className="contact-icons">
+				<EmailIcon className="icon2" />{" "}
+				<a
+					href="mailto:keshialeemartin@gmail.com"
+					target="_blank"
+					className="contact-link"
+				>
+					keshialeemartin@gmail.com
+				</a>
+			</div>
+			<div className="contact-me">
+				<form id="contact-form" onSubmit={handleSubmit}>
+					<div className="contact-label">
+						<label htmlFor="name">Name:</label>
+						<br />
+						<input
+							type="text"
+							defaultValue={name}
+							onChange={handleChange}
+							name="name"
+						/>
+					</div>
+					<div className="contact-label">
+						<label htmlFor="email">Email Address:</label>
+						<br />
+						<input
+							type="email"
+							defaultValue={email}
+							onChange={handleChange}
+							name="email"
+						/>
+					</div>
+					<div className="contact-label">
+						<label htmlFor="message">Message:</label>
+						<br />
+						<textarea
+							defaultValue={message}
+							onChange={handleChange}
+							name="message"
+							rows="3"
+						/>
+					</div>
+					<button type="submit">Submit</button>
+				</form>
+			</div>
 		</section>
 	);
 }
